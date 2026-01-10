790eaeee649b9f786283c9cb6bca5917ff83369e
harishnshetty <harishn662@gmail.com>
2026-01-10 22:12:58 +0530

    fix29

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -1,6 +1,6 @@
 def call() {
    withCredentials([usernamePassword(credentialsId: 'dockerhub-token', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
-        sh "echo \$DOCKER_PASS | docker login -u \$DOCKER_USER --password-stdin registry.hub.docker.com"
+        sh "echo \$DOCKER_PASS | docker login -u \$DOCKER_USER --password-stdin"
         sh "docker push ${env.dockerHubUsername}/${env.dockerImageName}:latest"
         sh "docker push ${env.dockerHubUsername}/${env.dockerImageName}:${env.BUILD_NUMBER}"
     }
