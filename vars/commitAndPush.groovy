8cc947dd2b2e70a1f6078e790fb6e8c7ff7c281a
harishnshetty <harishn662@gmail.com>
2026-01-10 22:44:44 +0530

    fix42

--- vars/commitAndPush.groovy
+++ vars/commitAndPush.groovy
@@ -1,7 +1,7 @@
 def call() {
-    sh "git config user.name \"${params.GitUserName}\""
-    sh "git config user.email \"${params.GitUserEmail}\""
+    sh "git config user.name \"${env.gitUserName}\""
+    sh "git config user.email \"${env.gitUserEmail}\""
     sh "git add ."
     sh "git commit -m \"Done by jenkins pipeline ${env.BUILD_NUMBER}\""
-    sh "git push https://${params.GitUserConfigName}:${params.GitPassword}@github.com/${params.GitUserName}/${params.GitRepo}.git"
+    sh "git push https://${env.gitUserConfigName}:${env.gitPassword}@github.com/${env.gitUserName}/${env.gitRepo}.git"
 }
