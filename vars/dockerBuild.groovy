0868939eac42c2c53b0af9f9d86ffff2defde060
harishnshetty <harishn662@gmail.com>
2026-01-10 21:58:29 +0530

    fix27

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,6 +1,6 @@
 def call() {
     // Build the Docker image
-    sh "docker build -t --no-cache ${env.dockerImageName} ."
+    sh "docker build -t ${env.dockerImageName} ."
      // Tag the Docker image
     sh "docker tag ${env.dockerImageName} ${env.dockerHubUsername}/${env.dockerImageName}:latest"
     sh "docker tag ${env.dockerImageName} ${env.dockerHubUsername}/${env.dockerImageName}:${env.BUILD_NUMBER}"
