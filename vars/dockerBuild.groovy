d3d1c00cff04806848c5827955c94880eb36a7b0
harishnshetty <harishn662@gmail.com>
2026-01-10 20:58:21 +0530

    fix21

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,9 +1,9 @@
-def call(String dockerHubUsername, String imageName) {
+def call() {
     // Build the Docker image
-    sh "docker build -t ${imageName} ."
+    sh "docker build -t ${env.imageName} ."
      // Tag the Docker image
-    sh "docker tag ${imageName} ${dockerHubUsername}/${imageName}:latest"
-    sh "docker tag ${imageName} ${dockerHubUsername}/${imageName}:${env.BUILD_NUMBER}"
+    sh "docker tag ${env.imageName} ${env.dockerHubUsername}/${env.imageName}:latest"
+    sh "docker tag ${env.imageName} ${env.dockerHubUsername}/${env.imageName}:${env.BUILD_NUMBER}"
     // Push the Docker image 
     
     }
