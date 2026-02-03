1de362f9b9556229e08c4687dfa995d6e68913f1
harishnshetty <harishn662@gmail.com>
2026-02-03 13:07:40 +0530

    tpl fixed

--- vars/trivyFs.groovy
+++ vars/trivyFs.groovy
@@ -1,23 +1,3 @@
 def call() {
-    stage('Trivy FS Scan') {
-        steps {
-            script {
-                // Load HTML template from shared library
-                def htmlTemplate = libraryResource 'html.tpl'
-
-                // Write it to workspace
-                writeFile file: 'trivy-html.tpl', text: htmlTemplate
-            }
-
-            sh '''
-              trivy fs . \
-                --security-checks vuln \
-                --severity HIGH,CRITICAL \
-                --ignore-unfixed \
-                --format template \
-                --template @trivy-html.tpl \
-                -o trivyfs-report.html || true
-            '''
-        }
-    }
+    sh "trivy fs --format template --template '@html.tpl' -o trivyfs_${env.BUILD_NUMBER}.html ."
 }
