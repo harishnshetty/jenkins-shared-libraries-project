04586400b119a02f4a6e29683cb57327930857f3
harishnshetty <harishn662@gmail.com>
2026-02-06 16:24:03 +0530

    post email3

--- vars/zpostemail.groovy
+++ vars/zpostemail.groovy
@@ -1,9 +1,12 @@
 def call(String buildStatus, String emailAddress) {
+
+    def buildUser = currentBuild.getBuildCauses('hudson.model.Cause$UserIdCause')[0]?.userId ?: 'GitHub/User Trigger'
+
+
     emailext(
         subject: "Pipeline ${buildStatus}: ${env.JOB_NAME} #${env.BUILD_NUMBER}",
         body: """
             <p>Project: ${env.JOB_NAME}</p>
-            <p> Build Name: ${env.JOB_NAME}</p>
             <p>Build Number: ${env.BUILD_NUMBER}</p>
             <p>Status: ${buildStatus}</p>
             <p>Started by: ${env.BUILD_USER}</p>
