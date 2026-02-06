ecb9b7d8361fd38676379e92a061fb32a316c5fe
harishnshetty <harishn662@gmail.com>
2026-02-06 16:04:33 +0530

    post email

--- vars/zpostemail.groovy
+++ vars/zpostemail.groovy
@@ -1,21 +1,27 @@
-def call() {
-script{
- echo "Email Notification"
- emailext(
-                subject: "Pipeline ${buildStatus}: ${env.JOB_NAME} #${env.BUILD_NUMBER}",
-                body: """
-                    <p>Youtube Link :- https://www.youtube.com/@devopsHarishNShetty </p>                                     
-                    <p>Flipkart DevSecops CICD pipeline status.</p>
-                    <p>Project: ${env.JOB_NAME}</p>
-                    <p>Build Number: ${env.BUILD_NUMBER}</p>
-                    <p>Build Status: ${buildStatus}</p>
-                    <p>Started by: ${buildUser}</p>
-                    <p>Build URL: <a href="${env.BUILD_URL}">${env.BUILD_URL}</a></p>
-                """,
-                to: '${params.emailAddress}',
-                from: '${params.emailAddress}',
-                mimeType: 'text/html',
-                attachmentsPattern: 'reports/gitleaks-report.json'
-                    ) 
+def call(buildStatus, emailAddress) {
+
+    def buildUser = currentBuild.getBuildCauses('hudson.model.Cause$UserIdCause')[0]?.userId ?: 'GitHub User'
+
+    emailext(
+        subject: "Pipeline ${buildStatus}: ${env.JOB_NAME} #${env.BUILD_NUMBER}",
+        body: """
+            <p>Flipkart DevSecOps CICD pipeline status.</p>
+
+            <p>Project: ${env.JOB_NAME}</p>
+            <p>Build Number: ${env.BUILD_NUMBER}</p>
+            <p>Status: ${buildStatus}</p>
+            <p>Started by: ${buildUser}</p>
+
+            <p><a href="${env.BUILD_URL}">View Build</a></p>
+        """,
+        to: '${params.emailAddress}',
+        from: '${params.emailAddress}',
+        mimeType: 'text/html',
+        attachmentsPattern: 'reports/gitleaks-report.json'
+    )
+
 }
-}
+
+
+
+// mfygsfnjfrweaesx
