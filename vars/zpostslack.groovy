9f10de626ec36cbf458e1f4fdb0c7b5424a77fb7
harishnshetty <harishn662@gmail.com>
2026-02-06 15:45:58 +0530

    post slack

--- vars/zpostslack.groovy
+++ vars/zpostslack.groovy
@@ -1,10 +1,16 @@
 def call() {
-script{
- echo "Slack Notification"
- slackSend(
-    channel:params.slackChannel,
-    color: COLOR_MAP[currentBuild.currentResult],
-    message: "*${currentBuild.currentResult}:* Job ${env.JOB_NAME} \n build ${env.BUILD_NUMBER} \n More info at: ${env.BUILD_URL}"
- )   
-}
+    def COLOR_MAP = [
+        'FAILURE' : 'danger',
+        'SUCCESS' : 'good',
+        'UNSTABLE': 'warning',
+        'ABORTED' : 'warning'
+    ]
+    
+    echo "Slack Notification"
+    slackSend(
+        channel: params.slackChannel,
+        color: COLOR_MAP[currentBuild.currentResult] ?: 'warning',
+        botUser: true,
+        message: "*${currentBuild.currentResult}:* Job ${env.JOB_NAME} \n build ${env.BUILD_NUMBER} \n More info at: ${env.BUILD_URL}"
+    )   
 }
