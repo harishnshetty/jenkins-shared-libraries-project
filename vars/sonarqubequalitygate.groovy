724d70d7a3fe8648d29a84eec78b4f78821c1e7c
harishnshetty <harishn662@gmail.com>
2026-02-06 16:59:41 +0530

    gate fixed

--- vars/sonarqubequalitygate.groovy
+++ vars/sonarqubequalitygate.groovy
@@ -1,11 +1,10 @@
 def call(String sonarqubeCredentialsId) {
-    scannerSonarqubeQualityGate(sonarqubeCredentialsId)
     timeout(time: 3, unit: 'MINUTES') {
-    script{
-        def qualityGate = waitForQualityGate()
-        if (qualityGate.status != 'OK') {
-            error "SonarQube quality gate failed: ${qualityGate.status}"
+        script{
+            def qualityGate = waitForQualityGate()
+            if (qualityGate.status != 'OK') {
+                error "SonarQube quality gate failed: ${qualityGate.status}"
+            }
         }
     }
-}
 }
