7c71ae0acb66d09db4e60fad82150083d559dc6a
harishnshetty <harishn662@gmail.com>
2026-02-06 16:20:25 +0530

    post email2

--- vars/zpostemail.groovy
+++ vars/zpostemail.groovy
@@ -1,21 +1,17 @@
-def call(String buildStatus) {
-
-    def buildUser = currentBuild.getBuildCauses('hudson.model.Cause$UserIdCause')[0]?.userId ?: 'GitHub User'
-
+def call(String buildStatus, String emailAddress) {
     emailext(
         subject: "Pipeline ${buildStatus}: ${env.JOB_NAME} #${env.BUILD_NUMBER}",
         body: """
-            <p>Flipkart DevSecOps CICD pipeline status.</p>
-
             <p>Project: ${env.JOB_NAME}</p>
+            <p> Build Name: ${env.JOB_NAME}</p>
             <p>Build Number: ${env.BUILD_NUMBER}</p>
             <p>Status: ${buildStatus}</p>
-            <p>Started by: ${buildUser}</p>
+            <p>Started by: ${env.BUILD_USER}</p>
 
             <p><a href="${env.BUILD_URL}">View Build</a></p>
         """,
-        to: '${params.emailAddress}',
-        from: '${params.emailAddress}',
+        to: "${emailAddress}",
+        from: "${emailAddress}",
         mimeType: 'text/html',
         attachmentsPattern: 'reports/gitleaks-report.json'
     )
