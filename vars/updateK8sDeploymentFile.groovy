86997cd9e56d6da1511a5cc5a1cee5ad4cdc4739
harishnshetty <harishn662@gmail.com>
2026-02-06 10:45:15 +0530

    update fixed

--- vars/updateK8sDeploymentFile.groovy
+++ vars/updateK8sDeploymentFile.groovy
@@ -1,8 +1,8 @@
 def call() {
     withCredentials([usernamePassword(credentialsId: 'github-token', usernameVariable: 'gitUserConfigName', passwordVariable: 'gitUserConfigEmail')]) {
     sh """
-    git config --global user.name ${env.gitUserConfigName}
-    git config --global user.email ${env.gitUserConfigEmail}
+    git config --local user.name ${env.gitUserConfigName}
+    git config --local user.email ${env.gitUserConfigEmail}
     git checkout ${env.BRANCH}
     sed -i 's|image: .*|image: ${env.dockerHubUsername}/${env.dockerImageName}:${env.BUILD_NUMBER}|' ${env.MANIFESTFILENAME}
     git add ${env.MANIFESTFILENAME} || true
