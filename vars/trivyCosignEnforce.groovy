4babb548c95a5466876dcf68a341687d227cb3d5
harishnshetty <harishn662@gmail.com>
2026-02-06 17:36:50 +0530

    singed

--- vars/trivyCosignEnforce.groovy
+++ vars/trivyCosignEnforce.groovy
@@ -1,38 +1,3 @@
-// def call(Map config) {
-//   withCredentials([
-//     string(credentialsId: 'COSIGN_KEY_B64', variable: 'COSIGN_KEY_B64')
-//   ]) {
-//     sh """
-//       set -e
-//       export COSIGN_EXPERIMENTAL=1
-
-//       echo "🔐 Reconstructing cosign private key"
-//       echo "\$COSIGN_KEY_B64" | base64 -d > cosign.key
-//       chmod 600 cosign.key
-
-//       echo "🔍 Generating SBOM using Trivy"
-//       trivy image \
-//         --format cyclonedx \
-//         --output sbom.cdx.json \
-//         ${config.image}
-
-//       echo "🧾 Attesting SBOM"
-//       cosign attest \
-//         --key cosign.key \
-//         --predicate sbom.cdx.json \
-//         --type cyclonedx \
-//         ${config.image}
-
-//       echo "✍️ Signing image"
-//       cosign sign \
-//         --key cosign.key \
-//         ${config.image}
-        
-//       echo "🧹 Cleanup"
-//       rm -f cosign.key sbom.cdx.json
-//     """
-//   }
-// }
 def call() {
     withCredentials([
         file(credentialsId: 'COSIGN_PRIVATE_KEY', variable: 'COSIGN_KEY_FILE'),
