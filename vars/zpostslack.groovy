6a1bbf08045590743fb6b7f3f2db09a62699f229
harishnshetty <harishn662@gmail.com>
2026-02-06 15:57:15 +0530

    post slack2

--- vars/zpostslack.groovy
+++ vars/zpostslack.groovy
@@ -1,11 +1,12 @@
-def call() {
+def call(buildStatus) {
 
-    def buildStatus = currentBuild.currentResult ?: 'SUCCESS'
+    // def buildStatus = currentBuild.currentResult ?: 'SUCCESS'
 
     slackSend(
         channel: params.slackChannel,
         color: buildStatus == 'SUCCESS' ? 'good' : 'danger',
         message: """
+                Build status: ${buildStatus}
                 Job: ${env.JOB_NAME}
                 Build: #${env.BUILD_NUMBER}
                 Status: ${buildStatus}
